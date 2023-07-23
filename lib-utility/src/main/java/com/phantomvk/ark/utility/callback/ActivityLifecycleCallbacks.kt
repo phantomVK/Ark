012bc9ff855a6be9d1d8e6678184cb64a01ed1f4
tanwenkang <tanwenkang@kuaishou.com>
2023-07-23 22:18:28 +0800

    Assert ActivityLifecycleCallbacks methods run on the main thread.

--- lib-utility/src/main/java/com/phantomvk/ark/utility/callback/ActivityLifecycleCallbacks.kt
+++ lib-utility/src/main/java/com/phantomvk/ark/utility/callback/ActivityLifecycleCallbacks.kt
@@ -3,49 +3,52 @@ package com.phantomvk.ark.utility.callback
 import android.app.Activity
 import android.app.Application.ActivityLifecycleCallbacks
 import android.os.Bundle
-import androidx.annotation.MainThread
+import com.phantomvk.ark.utility.sys.assertRunOnMainThread
 
 object ActivityLifecycleCallbacks : ActivityLifecycleCallbacks {
 
-  private val list = ArrayList<ActivityLifecycleCallbacks>()
+  private val set = HashSet<ActivityLifecycleCallbacks>()
 
-  @MainThread
-  @JvmStatic
   fun addCallback(callback: ActivityLifecycleCallbacks) {
-    list.add(callback)
+    assertRunOnMainThread()
+    set.add(callback)
   }
 
-  @MainThread
-  @JvmStatic
   fun removeCallback(callback: ActivityLifecycleCallbacks) {
-    list.remove(callback)
+    assertRunOnMainThread()
+    set.remove(callback)
+  }
+
+  fun containCallback(callback: ActivityLifecycleCallbacks): Boolean {
+    assertRunOnMainThread()
+    return set.contains(callback)
   }
 
   override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-    list.forEach { it.onActivityCreated(activity, savedInstanceState) }
+    set.forEach { it.onActivityCreated(activity, savedInstanceState) }
   }
 
   override fun onActivityStarted(activity: Activity) {
-    list.forEach { it.onActivityStarted(activity) }
+    set.forEach { it.onActivityStarted(activity) }
   }
 
   override fun onActivityResumed(activity: Activity) {
-    list.forEach { it.onActivityResumed(activity) }
+    set.forEach { it.onActivityResumed(activity) }
   }
 
   override fun onActivityPaused(activity: Activity) {
-    list.forEach { it.onActivityPaused(activity) }
+    set.forEach { it.onActivityPaused(activity) }
   }
 
   override fun onActivityStopped(activity: Activity) {
-    list.forEach { it.onActivityStopped(activity) }
+    set.forEach { it.onActivityStopped(activity) }
   }
 
   override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-    list.forEach { it.onActivitySaveInstanceState(activity, outState) }
+    set.forEach { it.onActivitySaveInstanceState(activity, outState) }
   }
 
   override fun onActivityDestroyed(activity: Activity) {
-    list.forEach { it.onActivityDestroyed(activity) }
+    set.forEach { it.onActivityDestroyed(activity) }
   }
 }
--- lib-utility/src/main/java/com/phantomvk/ark/utility/sys/MainThread.kt
+++ lib-utility/src/main/java/com/phantomvk/ark/utility/sys/MainThread.kt
@@ -16,3 +16,9 @@ internal val mainHandler = Handler(Looper.getMainLooper())
 fun postOnMainThread(delayMillis: Long = 0L, runnable: () -> Unit) {
   mainHandler.postDelayed(runnable, delayMillis)
 }
+
+fun assertRunOnMainThread() {
+  if (Looper.myLooper() != Looper.getMainLooper()) {
+    throw RuntimeException("Should run on the main thread.")
+  }
+}
